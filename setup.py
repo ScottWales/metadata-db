017e2e98be826856dc129eb460d0a880a3773b26
Scott Wales <scott.wales@unimelb.edu.au>
2018-03-13 16:51:41 +1100

    Install netcdf4

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         install_requires=[
             'sqlalchemy',
             'xarray',
+            'netcdf4',
             ],
         entry_points={
             'console_scripts': [
