3c7456e452b7fe294e37827b7d3716108baeb026
Scott Wales <scott.wales@unimelb.edu.au>
2018-03-13 15:51:22 +1100

    Initial skeleton

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+        name = 'metadb',
+        packages = find_packages('src'),
+        package_dir = {'': 'src'},
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+
+        install_requires = [
+            'sqlalchemy',
+            ],
+        entry_points = {
+            'console_scripts': [
+                ]}
+        )
